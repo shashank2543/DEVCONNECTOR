c4050cdcb53d76ac26f97aaedde3d1544219bd5c
shashank2543 <shashank.gupta@girnarsoft.com>
2019-07-23 16:00:23 +0530

    fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,12 +4,12 @@ const config = require('config');
 module.exports = function(req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
-
   // Check if not token
   if (!token) {
     res.status(401).json({
       msg: 'No Token'
     });
+    next();
   }
 
   // Verify token
