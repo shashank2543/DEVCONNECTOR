31ee0a94e8b059a7a8b6f2f179057cf4c25ea457
shashank2543 <shashank.gupta@girnarsoft.com>
2019-08-05 16:33:11 +0530

    inital setup

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,5 +1,5 @@
 import React,{useState,Fragment} from 'react'
-import {withRouter} from 'react-router-dom';
+import {withRouter,Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {createProfile} from '../../actions/profile';
