651769a8918e0cdd0604069ce073b68e8190e334
shashank2543 <shashank.gupta@girnarsoft.com>
2019-08-07 13:22:25 +0530

    Fix issue of routes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 const Landing = ({isAuthenticated}) => {
     if(isAuthenticated){
-     return <Redirect to="/dashboard"/>
+     return <Redirect to="/posts"/>
 
     }
     return (
